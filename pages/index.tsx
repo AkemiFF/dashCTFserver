f149917f89f7eacbeeb1b951211cc40d27e31139
Brad Akemi <mirado.akemi@gmail.com>
2025-02-06 11:57:22 +0300

    pages

---
+++ pages/index.tsx
@@ -0,0 +1,42 @@
+import type { GetServerSideProps, InferGetStaticPropsType } from 'next'
+
+import DashboardPage from '@/components/pages/dashboard-page'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import Head from 'next/head'
+
+
+
+type Props = {
+  // Add custom props here
+}
+
+const Homepage = (
+  _props: InferGetStaticPropsType<typeof getServerSideProps>
+) => {
+
+  return (
+    <>
+      <Head>
+        <title>Hackitech.co</title>
+      </Head>
+      <div className="min-h-screen bg-black text-white overflow-hidden relative">
+        <div className="relative z-10">
+          <DashboardPage />
+        </div>
+      </div>
+    </>
+  )
+}
+
+// or getServerSideProps: GetServerSideProps<Props> = async ({ locale })
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  locale,
+}) => ({
+  props: {
+    ...(await serverSideTranslations(locale ?? 'en', [
+      'common',
+    ])),
+  },
+})
+
+export default Homepage
