3d2be9b502a951f61fb33a8544e4f9103f93884d
Brad Akemi <mirado.akemi@gmail.com>
2025-03-12 20:31:10 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 // @ts-check
-const { i18n } = require('./next-i18next.config.js')
 
 // You can remove the following 2 lines when integrating our example.
-const { loadCustomBuildParams } = require('./next-utils.config')
+import { loadCustomBuildParams } from './next-utils.config.js';
 const { esmExternals = false, tsconfigPath } =
   loadCustomBuildParams()
 
@@ -21,18 +20,11 @@ const nextConfig = {
     tsconfigPath,
   },
   webpack: (config) => {
-    // Handle CSS imports from @uiw/react-md-editor
-    // config.module.rules.push({
-    //   test: /\.css$/i,
-    //   issuer: { and: [/\.(js|ts|md)x?$/] },
-    //   use: ['style-loader', 'css-loader'],
-    // });
 
-    // This is necessary for the markdown editor to work properly
     config.resolve.fallback = { fs: false };
 
     return config;
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
